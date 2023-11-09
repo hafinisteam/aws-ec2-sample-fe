35733797e18cd9d3c567694e167d7119d9065f28
ha.nguyen <hafinisteam@gmail.com>
2023-11-09 11:15:25 +0700

    change api path

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import useSWR from 'swr'
 import axios from 'axios'
 import { useState } from 'react'
 
-const URL = 'http://localhost:3001/users'
+const URL = import.meta.env.PROD ? '/api/users' : 'http://localhost:3001/api/users'
 
 const fetcher = (url: string) => axios(url).then((res) => res.data)
 
